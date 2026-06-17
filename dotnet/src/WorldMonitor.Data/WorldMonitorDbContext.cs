be5a01364c1f3d0b6aa9c27deb268624e7124082
capnbigal <48189399+capnbigal@users.noreply.github.com>
2026-06-16 22:14:07 -0400

    feat(data): wire Identity/Watchlist DbSets + AddIdentityWatchlist migration

--- dotnet/src/WorldMonitor.Data/WorldMonitorDbContext.cs
+++ dotnet/src/WorldMonitor.Data/WorldMonitorDbContext.cs
@@ -1,11 +1,16 @@
 using Microsoft.EntityFrameworkCore;
 using WorldMonitor.Data.Entities;
+using WorldMonitor.Data.Entities.Identity;
+using WorldMonitor.Data.Entities.Watchlist;
 
 namespace WorldMonitor.Data;
 
 public class WorldMonitorDbContext(DbContextOptions<WorldMonitorDbContext> options) : DbContext(options)
 {
     public DbSet<CacheEntry> CacheEntries => Set<CacheEntry>();
+    public DbSet<User> Users => Set<User>();
+    public DbSet<UserPreference> UserPreferences => Set<UserPreference>();
+    public DbSet<FollowedCountry> FollowedCountries => Set<FollowedCountry>();
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
